f8b759e7c2d56014e7f2422f787160310a09c7ef
dipp314 <owens.d.seth@gmail.com>
2026-03-31 17:24:19 -0500

    Update Indexer.java

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -8,17 +8,27 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 // imports the variable for the motor port
 import static frc.robot.Ports.IndexerPorts.kIndexerMotor;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 // imports the class that helps control the motor
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class Indexer extends SubsystemBase {
     // motor initialization
     SparkFlex indexerMotor = new SparkFlex(kIndexerMotor.ID, MotorType.kBrushless);
-
+    SparkClosedLoopController indexerMotorController = indexerMotor.getClosedLoopController();
     Indexer() {
-       SparkFlexConfig config = new SparkFlexConfig();
+        SparkFlexConfig config = new SparkFlexConfig();
+        config.idleMode(IdleMode.kBrake);
+        indexerMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    public void runMotor() {
+
     }
 
     @Override
